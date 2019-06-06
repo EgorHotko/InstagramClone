79bf1f40993b5cc702adeae4f92f23843c35580c
unknown <egorhotko@gmail.com>
2019-06-06 20:20:15 +0300

    created views router, added main page

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
+import * as path from 'path';
 import postsRouter from './routes/posts';
 import usersRouter from './routes/users';
 import authRouter from './routes/auth';
 import commentRouter from './routes/comment';
 import hashtagRouter from './routes/hashtag';
+import viewsRouter from './routes/views';
 import { database } from './db/database';
 import * as cookieParser from 'cookie-parser';
 
@@ -11,8 +13,14 @@ import * as cookieParser from 'cookie-parser';
 const app: express.Application = express();
 database.sync();
 
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 app.use(cookieParser());
+app.use('/', viewsRouter);
 app.use('/api/posts', postsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/comments', commentRouter);
